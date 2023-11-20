9bc5c4d8ffb998aea5b45efe77fb9a2a16e37202
MrSushl <96308156+MrSushl@users.noreply.github.com>
2023-11-20 04:45:29 +0300

    exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,19 +3,19 @@ from sample_config import Config
 
 class Development(Config):
 
-    APP_ID = 
+    APP_ID = "14930661"
     
-    API_HASH = ""
+    API_HASH = "f0536a5c2ad076b0b648db5938cabe90"
 
-    ALIVE_NAME = ""
+    ALIVE_NAME = "?>"
 
-    DB_URI = ""
+    DB_URI = "mongodb+srv://mrsushl:7Fcbd82ae7@cluster0.daecwsn.mongodb.net/"
 
-    STRING_SESSION = ""
+    STRING_SESSION = "1BJWap1sBu314fv-CdKofuE6pcblFHbRhu1JiaMETKxaCRZR4IGfccqQVgEIeF3q7j0XJQaZ_hqkq4cvwP8SxGnHEcH0vKFn5iTYO6IVO3g6hPSaoAwkqT8D82geJ7zqglUT60c-lEzADUfTMOcoFaZOkAWl96wiVgG9i9Td7FNX0wH1ENww88oSXBIdNSnlBuZFeKEEdlCfThz_8vj3njA_9Dxo2cY43ihDk54ND9tfjGXRTYooJ6HRGw1JZopxCbsVamjzySaAwIKoanbIHeUxAkbCedM5OindQiBj9bmtSEGgwnUvDxKlHLxJqGBjr9doeLvhF_HvDQNqjhHRyLXR7KDp5a80="
 
-    TG_BOT_TOKEN = ""
+    TG_BOT_TOKEN = "6884512758:AAFz_B9NKRsIxdLfXMTrlJArqBDqxU_RTGU"
 
-    PRIVATE_GROUP_BOT_API_ID = 
+    PRIVATE_GROUP_BOT_API_ID = "-1002124920969"
 
     COMMAND_HAND_LER = "."
 
